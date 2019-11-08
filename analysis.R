1fc97be63afa20a376ae94566758228d12dc62c8
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2019-11-08 09:01:25 -0500

    add URL processing

--- analysis.R
+++ analysis.R
@@ -4,12 +4,16 @@ library(tidyverse)
 library(lubridate)
 library(ggplot2)
 
-pre <- read_rds("processed-tweets.rds")
+pre <- read_rds("processed-data/processed-data.rds")
+
+pre <- pre %>% unnest(urls_expanded_url)
+
+shorteners <- c("bit.ly", "ow.ly", "buff.ly", "goo.gl", "ln.is", "tinyurl.com", "share.es", "ht.ly", "fb.me", "wp.me", "ift.tt")
 
 # flat <- flatten(pre)
 
 #get number of tweets by username over 2 tweets
-user_tweet_counts <- flat %>% 
+user_tweet_counts <- pre %>% 
   filter(!is_retweet) %>% 
   count(screen_name) %>% 
   arrange(desc(n)) %>%
@@ -39,6 +43,22 @@ by_week <- tweets %>%
   mutate(x = row_number()) %>% 
   summarise(max = max(x)) %>% 
   arrange(desc(max))
+
+# process URLs
+
+pre <- pre %>% 
+  mutate(domains = urltools::domain(d$urls_expanded_url),
+         need_to_expand = domains %in% shorteners)
+
+pre_expanded <- pre %>% 
+  mutate(processed_url = ifelse(need_to_expand == 1, expand_urls(urls_expanded_url), urls_expanded_url))
+
+pre_expanded <- pre_expanded %>% 
+  mutate(domain = domain(urls_expanded_url),
+         is_github_link = str_detect(domain, "github.com") & !str_detect(domain, "gist"),
+         is_gist_link = str_detect(domain, "gist.github.com"))
+
+write_rds(pre_expanded, "/processed-data/processed_data_with_processed_urls.rds")
 
 #dowload images organized into folders by username, week-number as prefix, for anyone over n contributions
 
