4f23984d798ab13e29bbaf20ff2dbb97ab284fe1
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2019-11-12 14:30:37 -0500

    add tidycode processing code and data

---
+++ tidycode-analysis.R
@@ -0,0 +1,40 @@
+# tidycode-analysis
+
+library(tidycode)
+library(tidyverse)
+
+# dir <-"/Users/jrosenb8/github-content-scraper/cache"
+# 
+# full_dir <- list.files(dir, full.names = T)
+# 
+# full_dir <- full_dir[tools::file_ext(full_dir) == "R"]
+# 
+# l <- full_dir %>%
+#   map_df(possibly(read_rfiles, data.frame(file = ., expr = NA, line = NA)))
+# 
+# f <- function(x) {
+#   NA
+# }
+# 
+# is_null <- function(x) {
+#   x == "NULL"
+# }
+# 
+# bool <- l$expr %>% 
+#   modify_if(is.language, f) %>% 
+#   is.na()
+# 
+# ll <- l[bool, ] %>% 
+#   filter(expr != "NULL") %>% 
+#   unnest_calls(expr)
+# 
+# write_rds(ll, "processed-data/processed-tidycode-data.rds")
+
+ll <- read_rds("processed-data/processed-tidycode-data.rds")
+
+lll <- ll %>% 
+  anti_join(get_stopfuncs())
+
+lll %>% 
+  inner_join(get_classifications("crowdsource", include_duplicates = FALSE)) %>%
+  select(func, classification)
